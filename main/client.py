0187710ed35110dc02e5bbf4342d27cfae142813
Buddy <badinlu.amirhossein@gmail.com>
2024-07-07 14:57:12 +0330

    fixed

---
+++ main/client.py
@@ -0,0 +1,84 @@
+import socket
+import threading
+import select
+import queue
+from utils import *
+from getch import getch
+
+class ClientState:
+    def __init__(self):
+        self.exit_event = threading.Event()
+        self.input_queue = queue.Queue()
+
+class InputHandler:
+    def __init__(self, state):
+        self.state = state
+
+    def run(self):
+        try:
+            while not self.state.exit_event.is_set():
+                char = getch()
+                if char:
+                    if ord(char) == 3:
+                        char = 'q'
+                    self.state.input_queue.put(char)
+                    if char == 'q':
+                        break
+        except Exception as e:
+            stdio_print(f"Error in InputHandler: {e}")
+
+class SocketHandler:
+    def __init__(self, client_socket, state):
+        self.client_socket = client_socket
+        self.state = state
+
+    def run(self):
+        try:
+            while not self.state.exit_event.is_set():
+                readable, writable, _ = select.select([self.client_socket], [self.client_socket], [self.client_socket], 0.5)
+                
+                if readable:
+                    response = self.client_socket.recv(1024)
+                    if not response:
+                        self.state.exit_event.set()
+                        raise ConnectionResetError()
+                    stdio_print(f"Server: {response.decode()}")
+                
+                if writable and not self.state.input_queue.empty():
+                    char = self.state.input_queue.get()
+                    self.client_socket.sendall(char.encode())
+                    if char == 'q':
+                        self.state.exit_event.set()
+                        break
+        except ConnectionResetError:
+            stdio_print("Server disconnected you")
+
+class Client:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.state = ClientState()
+
+    def start(self):
+        self.client_socket.connect((self.server_address, self.server_port))
+        stdio_print(f"Connected to {self.server_address}:{self.server_port}")
+
+        input_handler = InputHandler(self.state)
+        socket_handler = SocketHandler(self.client_socket, self.state)
+
+        input_thread = threading.Thread(target=input_handler.run)
+        socket_thread = threading.Thread(target=socket_handler.run)
+        
+        input_thread.start()
+        socket_thread.start()
+        
+        input_thread.join()
+        socket_thread.join()
+
+        self.client_socket.close()
+        stdio_print("Client socket closed")
+
+if __name__ == "__main__":
+    client = Client(server_address='127.0.0.1', server_port=8080)
+    client.start()
---
+++ main/server.py
@@ -0,0 +1,98 @@
+import socket
+import threading
+import select
+from utils import *
+
+class ClientHandler:
+    def __init__(self, client_socket, addr, idle_timeout, disconnect_event):
+        self.client_socket = client_socket
+        self.addr = addr
+        self.idle_timeout = idle_timeout
+        self.disconnect_event = disconnect_event
+
+    def handle(self):
+        stdio_print(f"Handling client {self.addr}")
+
+        self.client_socket.setblocking(False)
+        timeout = 0
+        while True:
+            try:
+                readable, _, _ = select.select([self.client_socket], [], [], 0.5)
+                timeout += 0.5
+                if self.disconnect_event.is_set():
+                    break
+                if readable:
+                    timeout = 0
+                    request = self.client_socket.recv(1024)
+                    if not request:
+                        raise ConnectionResetError()
+
+                    char = request.decode()
+                    stdio_print(f"Received from {self.addr}: {char}")
+
+                    if char == 'q':
+                        stdio_print(f"Client {self.addr} disconnected")
+                        self.client_socket.sendall("Connection closed by client".encode())
+                        break
+
+                    message = f"Received {char}"
+                    self.client_socket.sendall(message.encode())
+                elif timeout >= self.idle_timeout:
+                    stdio_print(f"Client {self.addr} disconnected due to inactivity")
+                    self.client_socket.sendall("Disconnected due to inactivity".encode())
+                    break
+            except ConnectionResetError:
+                stdio_print(f"Client {self.addr} error connection")
+                break
+
+        self.cleanup()
+
+    def cleanup(self):
+        self.client_socket.close()
+        ServerState.remove_client(self.client_socket)
+
+class ServerState:
+    client_events = {}
+
+    @classmethod
+    def add_client(cls, client_socket, event):
+        cls.client_events[client_socket] = event
+
+    @classmethod
+    def remove_client(cls, client_socket):
+        del cls.client_events[client_socket]
+
+    @classmethod
+    def disconnect_all(cls):
+        for event in cls.client_events.values():
+            event.set()
+
+class Server:
+    def __init__(self, host, port, max_clients, idle_timeout):
+        self.host = host
+        self.port = port
+        self.max_clients = max_clients
+        self.idle_timeout = idle_timeout
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(self.max_clients)
+        stdio_print(f"Listening on {self.host}:{self.port}")
+
+        try:
+            while True:
+                client_socket, addr = self.server_socket.accept()
+                stdio_print(f"Accepted connection from {addr}")
+                client_event = threading.Event()
+                ServerState.add_client(client_socket, client_event)
+                client_handler = ClientHandler(client_socket, addr, self.idle_timeout, client_event)
+                threading.Thread(target=client_handler.handle).start()
+        except KeyboardInterrupt:
+            stdio_print("\nShutting down...")
+            ServerState.disconnect_all()
+            self.server_socket.close()
+
+if __name__ == "__main__":
+    server = Server(host='0.0.0.0', port=8080, max_clients=5, idle_timeout=10)
+    server.start()
